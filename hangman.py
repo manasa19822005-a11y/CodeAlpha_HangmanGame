ef79f3fbd731a187ad73530350c586ea8c7f0d2b
manasa19822005-a11y <manasa19822005@gmail.com>
2026-05-24 10:36:48 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,37 @@
+import random
+
+words = ["apple", "chair", "tiger", "green", "house"]
+
+word = random.choice(words)
+
+guessed = []
+attempts = 6
+
+while attempts > 0:
+
+    display = ""
+
+    for letter in word:
+        if letter in guessed:
+            display += letter + " "
+        else:
+            display += "_ "
+
+    print(display)
+
+    if "_" not in display:
+        print("You Won!")
+        break
+
+    guess = input("Guess a letter: ")
+
+    if guess in word:
+        guessed.append(guess)
+    else:
+        attempts -= 1
+        print("Wrong Guess")
+        print("Attempts Left:", attempts)
+
+if attempts == 0:
+    print("You Lost!")
+    print("Word was:", word)
